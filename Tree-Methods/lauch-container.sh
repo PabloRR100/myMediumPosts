da50cc8c2597188afa783b49a5987f23c4d46d9e
PabloRR100 <pabloruizruiz10@gmail.com>
2020-03-31 19:39:17 +0200

    starting to plot correctly decision regions on 3d

--- Tree-Methods/lauch-container.sh
+++ Tree-Methods/lauch-container.sh
@@ -1,1 +1,7 @@
-docker run -it -d --rm --name ds-testing -v $PWD:/app -p 8888:8888 datascience bash
+PORT=8282
+docker run -it -d \
+    --rm --name ds-testing \
+    -v $PWD:/app \
+    -e SERVING_PORT=$PORT \
+    -p $PORT:$PORT \
+    datascience
